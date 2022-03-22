38765ec50c021345c2f181d4d7a57b6bacd98835
Marek Vospěl <marek@vospel.cz>
2022-03-22 19:00:04 +0100

    fix(backend): serve correct vue files

--- apps/backend/src/router/index.ts
+++ apps/backend/src/router/index.ts
@@ -4,14 +4,18 @@ import path from 'path'
 
 const resolve = (p) => path.resolve(__dirname, p)
 
-
 export const router = Router()
 
-const staticHandler = serveStatic(resolve('../../../frontend/dist'), { index: ['index.html'] })
+const staticHandler = serveStatic(resolve('../../../frontend/dist'))
 
 router.use((req, res, next) => {
   if (req.originalUrl.startsWith('/api')) next()
   else staticHandler(req, res, next)
+})
+
+router.use((req, res, next) => {
+  if (req.originalUrl.startsWith('/api')) next()
+  else res.sendFile(path.join(__dirname, '../../../frontend/dist/index.html'))
 })
 
 router.use('/api', apiRouter)
