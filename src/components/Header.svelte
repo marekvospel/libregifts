158de1a06a15492a36f1af586391d16434a5493e
Marek Vospěl <marek@vospel.cz>
2023-11-22 11:37:23 +0100

    chore: change size of the logo

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,7 +4,7 @@
 
     <a href='/' class='flex flex-row gap-4 lg:gap-8'>
 
-      <img src='/znak.webp' alt='' class='flex-shrink-0'>
+      <img src='/znak.webp' alt='' class='flex-shrink-0 h-20'>
 
       <div class='flex flex-col justify-center'>
         <h1 class='text-xl font-extrabold'>Strom splněných přání Dolní Břežany 2023</h1>
