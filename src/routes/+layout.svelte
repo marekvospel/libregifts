76dac1cb72c6b1751c82400c3eb3f12f2c2dc015
Marek Vospěl <marek@vospel.cz>
2023-11-22 10:50:54 +0100

    fix: images

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
   import Header from '../components/Header.svelte'
-  import Image from '$/assets/img/dolnibrezany.jpg'
+  import imagePath from '$/assets/img/dolnibrezany.jpg'
+  import imageBlurredPath from '$/assets/img/dolnibrezany-blurred.jpg'
 </script>
 
 
 <div class='relative w-full max-w-screen overflow-hidden min-h-screen flex flex-col items-center'>
   <div class='container self-center px-5 sm:px-8 lg:px-32'>
     <Header />
-    <div class='absolute left-0 w-screen z-10 h-96 dolnibrezany-img'></div>
+    <div class='absolute left-0 w-screen z-10 h-96 dolnibrezany-img' style={`background-image: url("${imagePath}"), url("${imageBlurredPath}")`}></div>
     <div class='h-96 w-max'></div>
     <div class='relative -mt-12 z-20'>
       <slot />
@@ -22,7 +23,7 @@
 
   .dolnibrezany-img {
       width: calc(100vw - 12px);
-      background-image: url("$/assets/img/dolnibrezany.jpg"), url("$/assets/img/dolnibrezany-blurred.jpg");
+      /*background-image: url("$/assets/img/dolnibrezany.jpg"), url("$/assets/img/dolnibrezany-blurred.jpg");*/
       background-repeat: no-repeat, repeat;
       background-position: center, center;
       background-size: 100%, auto;
